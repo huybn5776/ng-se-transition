654f375a0fc961720312468dda5e91a487f7148d
huybn5776 <aseeme555076@gmail.com>
2019-09-01 22:01:56 +0800

    Remove unused imports.

--- src/lib/se-trans.module.ts
+++ src/lib/se-trans.module.ts
@@ -1,16 +1,8 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
-import { RoutingModule } from "../../demo/src/app/routing/routing.module";
-import { SeTransDirective } from "./se-trans.directive";
+import { SeTransDirective } from './se-trans.directive';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RoutingModule
-  ],
   declarations: [
     SeTransDirective
   ],
